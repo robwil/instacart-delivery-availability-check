5ae455f432ea0f726675279fe8f65b8d0671c289
Rob Williams <rwilliams@spotify.com>
2020-04-18 12:15:17 -0400

    add retry logic and disable voice notifications

--- check.py
+++ check.py
@@ -12,7 +12,7 @@ import sys
 credentials = json.load(open("credentials.json"))
 INSTACART_EMAIL = credentials["INSTACART_EMAIL"]
 INSTACART_PASSWORD = credentials["INSTACART_PASSWORD"]
-MAILGUN_URL = credentials["MAILGUN_DOMAIN"]
+MAILGUN_DOMAIN = credentials["MAILGUN_DOMAIN"]
 MAILGUN_API_KEY = credentials["MAILGUN_API_KEY"]
 STORE_LIST = credentials["STORE_LIST"]
 INSTACART_BASE_URL = credentials["INSTACART_BASE_URL"]
@@ -20,29 +20,34 @@ INSTACART_DELIVERY_URL = credentials["INSTACART_DELIVERY_URL"]
 NOTIFICATION_EMAIL = credentials["NOTIFICATION_EMAIL"]
 
 # -- login logic -- #
-print("Logging into Instacart...")
-driver = start_chrome(INSTACART_BASE_URL, headless=True)
-try:
-    f = open("cookies.pkl", "rb")
-    cookies = pickle.load(f)
-    print("Cookies found! Using cookies to log in.")
-    for cookie in cookies:
-        driver.add_cookie(cookie)
-    go_to(INSTACART_BASE_URL)
-except IOError:
-    print("Cookies not found, logging in!")
-    click(Button("Log In"))
-    write(INSTACART_EMAIL, into="Email address")
-    write(INSTACART_PASSWORD, into="Password")
-    click(Button("Log In"))
-    wait_until(Link("Your Items").exists)
-    pickle.dump( driver.get_cookies() , open("cookies.pkl","wb"))
+def login():
+    print("Logging into Instacart...")
+    try:
+        f = open("cookies.pkl", "rb")
+        cookies = pickle.load(f)
+        print("Cookies found! Using cookies to log in.")
+        for cookie in cookies:
+            driver.add_cookie(cookie)
+        go_to(INSTACART_BASE_URL)
+    except IOError:
+        print("Cookies not found, logging in!")
+        click(Button("Log In"))
+        write(INSTACART_EMAIL, into="Email address")
+        write(INSTACART_PASSWORD, into="Password")
+        click(Button("Log In"))
+        wait_until(Link("Your Items").exists)
+        pickle.dump( driver.get_cookies() , open("cookies.pkl","wb"))
 
-print("Checking available delivery slots...\n")
+driver = start_chrome(INSTACART_BASE_URL, headless=True)
+login()
 
 
 # -- check store logic -- #
-def check_delivery_times_for_store(store_name):
+def check_delivery_times_for_store(store_name, retries=0):
+    if retries > 2:
+        print("Already retried 2 times, giving up...")
+        return False, "Errored after 2 retries"
+
     sleep(2)
     go_to(INSTACART_DELIVERY_URL.format(store_name))
     sleep(2)
@@ -61,22 +66,25 @@ def check_delivery_times_for_store(store_name):
         return False, "No Delivery times available for {}".format(store_name)
     else:
         #unexpected response, generate screenshot, return generic error
-        return False, "Unexpected response"
+        print("Retrying {} time #{}".format(store_name, retries + 1))
+        driver.save_screenshot("screenshot_retry_{}.png".format(retries))
+        login()
+        return check_delivery_times_for_store(store_name, retries + 1)
 
 
 # -- send email -- #
 def send_simple_message(message):
 
-    if (MAILGUN_API_KEY=="") or (MAILGUN_API_KEY=="xxx") or (MAILGUN_URL=="") or (MAILGUN_URL=="xxx.mailgun.org") or (NOTIFICATION_EMAIL=="") or (NOTIFICATION_EMAIL=="xxx@gmail.com"):
+    if (MAILGUN_API_KEY=="") or (MAILGUN_API_KEY=="xxx") or (MAILGUN_DOMAIN=="") or (MAILGUN_DOMAIN=="xxx.mailgun.org") or (NOTIFICATION_EMAIL=="") or (NOTIFICATION_EMAIL=="xxx@gmail.com"):
         print ("ERROR: Can't sent email notification. Invalid Mailgun API Key, URL or Notification Email")
         return null
 
     return requests.post(
-        "https://api.mailgun.net/v3/{}/messages".format(MAILGUN_URL),
+        "https://api.mailgun.net/v3/{}/messages".format(MAILGUN_DOMAIN),
         auth=("api", MAILGUN_API_KEY),
         data={
             "from": "Instacart Delivery Notifier <instacart_notify@{}>".format(
-                MAILGUN_URL
+                MAILGUN_DOMAIN
             ),
             "to": NOTIFICATION_EMAIL,
             "subject": "Instacart Delivery Time Available!!",
@@ -89,7 +97,7 @@ def send_simple_message(message):
 def main():
     deliveryAvailability = False
     
-    voiceNotification = True
+    voiceNotification = False
     emailNotification = True
 
     while deliveryAvailability == False:
